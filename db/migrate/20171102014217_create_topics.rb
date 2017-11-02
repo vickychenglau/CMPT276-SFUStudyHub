cd23ca8eb987336ed679899ee9906de78feb1382
David Ham <dham@sfu.ca>
2017-11-01 18:45:57 -0700

    Databases are linked!

--- db/migrate/20171102014217_create_topics.rb
+++ db/migrate/20171102014217_create_topics.rb
@@ -1,10 +1,10 @@
 class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
+      t.string :description
+      t.string :title
       t.references :course, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
-      t.text :description
-      t.string :title
 
       t.timestamps null: false
     end
