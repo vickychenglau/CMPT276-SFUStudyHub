16c85e0277316e81e8efd52a83f7c3acb1cad53c
eha14 <eijialford@gmail.com>
2017-11-03 21:17:42 -0700

    added links for course id to messageboard

--- app/controllers/messageboard_controller.rb
+++ app/controllers/messageboard_controller.rb
@@ -1,9 +1,11 @@
 class MessageboardController < ApplicationController
   def index
+    # Maybe show this in the admin page?
     @topics = Topic.all
   end
 
   def show
+    # redirect_to(:controller => 'posts', :action => 'index')
   end
 
   def new
