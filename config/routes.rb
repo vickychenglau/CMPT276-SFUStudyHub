17147eaeadd10d3f1380fe9a7d39fefe0c1561d8
vcl2 <vcl2@sfu.ca>
2017-11-04 12:57:04 -0700

    commit before switching to postspageModelController branch

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,6 @@ Rails.application.routes.draw do
     resources :posts
   end
 
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
