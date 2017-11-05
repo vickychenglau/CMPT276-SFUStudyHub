37f4399428f2abce4c8ce0cf4383ecca8cbeea0e
eha14 <eijialford@gmail.com>
2017-11-05 11:54:14 -0800

    added new and create to messageboardcontroller

--- app/controllers/messageboard_controller.rb
+++ app/controllers/messageboard_controller.rb
@@ -10,6 +10,22 @@ class MessageboardController < ApplicationController
   end
 
   def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+    if @topic.save
+      redirect_to @topic
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def topic_params
+    # Also have to include that they are logged in
+    params.require(:topic).permit(:description, :title)
   end
 
 end
