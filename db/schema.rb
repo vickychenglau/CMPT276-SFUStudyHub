5a97a752cc022fcebfd25353bbbcb2b240b177cd
nlchao <nlchao@sfu.ca>
2017-11-18 22:47:44 -0800

    After merging with development-2

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20171118215322) do
-
 ActiveRecord::Schema.define(version: 20171118230825) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,7 +120,6 @@ ActiveRecord::Schema.define(version: 20171118230825) do
     t.string   "first_name"
     t.string   "email"
     t.string   "role"
-    t.string   "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
