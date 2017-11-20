3972de6bd3767bb45e39c1eee7ee85a791aeeac0
vcl2 <vcl2@sfu.ca>
2017-11-19 17:13:24 -0800

    Added some tests

--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,122 @@ ActiveRecord::Schema.define(version: 20171118230825) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "average_caches", force: :cascade do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "avg",           null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "term"
+  end
+
+  create_table "fbusers", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "username"
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.string   "role"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "overall_averages", force: :cascade do |t|
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "overall_avg",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.integer  "user_id"
+    t.text     "text"
+    t.integer  "rating"
+    t.boolean  "anon"
+    t.integer  "parent"
+    t.integer  "postable_id"
+    t.string   "postable_type"
+    t.boolean  "deleted"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "rates", force: :cascade do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type", using: :btree
+  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id", using: :btree
+
+  create_table "rating_caches", force: :cascade do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type", using: :btree
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "comment"
+    t.integer  "person_rating_id"
+    t.integer  "person_rated_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "description"
+    t.string   "title"
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.text     "first_post"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "topics", ["course_id"], name: "index_topics_on_course_id", using: :btree
+  add_index "topics", ["user_id"], name: "index_topics_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.string   "last_name"
+    t.string   "first_name"
+    t.string   "email"
+    t.string   "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_foreign_key "topics", "courses"
+  add_foreign_key "topics", "users"
 end
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -19,64 +19,112 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  fixtures :users
+  fixtures :users, :topics, :courses
 
   test "Checking to see if it posts blank" do
     post = Post.new
-    assert !post.valid?
+    assert !post.valid?, "Blank post is valid"
   end
 
   test "If rating is not there" do
     user=users(:regular)
-    post=Post.new(:text => "Some textlksdlkjasd", :user_id => 1, :anon => false , :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:text => "Some textlksdlkjasd", :user_id => 1, :topic_id => 1
+    :anon => false , :deleted => false)
     assert post.invalid?, "No post rating considered valid"
   end
 
 
   test "If text is more than 2 characters" do
     user=users(:regular)
-    post=Post.new(:rating => 5, :text => "Some textlksdlkjasd", :user_id => 1, :anon => false, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "Some textlksdlkjasd", :user_id => 1, :topic_id => 1
+    :anon => false , :deleted => false)
     assert post.valid?, "Post Text > 2 characters is invalid"
   end
 
   test "If text is 2 characters" do
     user=users(:regular)
-    post=Post.new(:rating => 5, :text => "So", :user_id => 1, :anon => false, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "So", :user_id => 1, :topic_id => 1
+    :anon => false, :deleted => false)
     assert post.valid?, "Post Text is 2 characters is invalid"
   end
 
   test "text is less than 2 characters" do
     user=users(:regular)
-    post=Post.new(:rating => 5, :text => "a", :user_id => 1, :anon => false, :deleted => false)
-    assert post.invalid?, "Post Text < 2 characters is invalid"
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "a", :user_id => 1, :topic_id => 1
+    :anon => false, :deleted => false)
+    assert post.invalid?, "Post Text < 2 characters is valid"
   end
 
   test "No user with the post" do
-    post=Post.new(:rating => 5, :text => "So222113222", :anon => false, :deleted => false)
+    post=Post.new(:rating => 5, :text => "So222113222", :topic_id => 1
+    :anon => false, :deleted => false)
     assert post.invalid?, "test is valid with no user attached to post"
+  end
+
+  test "No user with user ID" do
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "a", :user_id => 9, :topic_id => 1
+    :anon => false, :deleted => false)
+    assert post.invalid?, "test is valid with nonexistent user_id"
+  end
+
+  test "No topic with the post" do
+    user=users(:regular)
+    post=Post.new(:rating => 5, :text => "So222113222", :user_id => 1, :anon => false, :deleted => false)
+    assert post.invalid?, "test is valid with no topic attached to post"
+  end
+
+  test "No topic with topic ID" do
+    user=users(:regular)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "a", :user_id => 1, :topic_id => 9
+    :anon => false, :deleted => false)
+    assert post.invalid?, "test is valid with nonexistent topic_id"
   end
 
   test "Rating is 0" do
     user=users(:regular)
-    post=Post.new(:rating => 0, :text => "So222113222", :user_id => 1, :anon => false, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 0, :text => "So22", :user_id => 1, :topic_id => 1
+    :anon => false, :deleted => false)
     assert post.valid?, "Rating is zero is invalid"
   end
 
   test "Rating is below 0" do
     user=users(:regular)
-    post=Post.new(:rating => -3, :text => "So222113222", :user_id => 1, :anon => false, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => -3, :text => "So222", :user_id => 1, :topic_id => 1
+    :anon => false, :deleted => false)
     assert post.valid?, "Rating below 0 is invalid"
   end
 
   test "Rating is large " do
     user=users(:regular)
-    post=Post.new(:rating => 1000000, :text => "So222113222", :user_id => 1, :anon => false, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 1000000, :text => "So552", :user_id => 1, :topic_id => 1
+    :anon => false, :deleted => false)
     assert post.valid?, "Large rating is invalid"
   end
 
   test "Post as anonymous" do
     user=users(:regular)
-    post=Post.new(:rating => 5, :text => "So222113222", :user_id => 1, :anon => true, :deleted => false)
+    course=courses(:one)
+    topic=topics(:one)
+    post=Post.new(:rating => 5, :text => "So222113222", :user_id => 1, :topic_id => 1
+    :anon => true, :deleted => false)
     assert post.valid?, "Posting as anonymous fails"
   end
 
