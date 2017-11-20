00d035f6cc208f53d6c02515ae73750ec0c0380f
nlchao <nlchao@sfu.ca>
2017-11-20 15:15:08 -0800

    Fixed the stars showing without needing refreshment bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118230825) do
+ActiveRecord::Schema.define(version: 20171120040603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,9 @@ ActiveRecord::Schema.define(version: 20171118230825) do
     t.integer  "topic_id"
     t.integer  "user_id"
     t.text     "text"
+    t.integer  "rating"
     t.boolean  "anon"
+    t.integer  "parent"
     t.integer  "postable_id"
     t.string   "postable_type"
     t.boolean  "deleted"
