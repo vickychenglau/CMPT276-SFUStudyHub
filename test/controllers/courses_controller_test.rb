7774d3f9f4cb799bf3d390e6a3c73f66133543ee
Lambdah <eijialford@gmail.com>
2017-11-19 18:10:58 -0800

    user control tests

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 class CoursesControllerTest < ActionController::TestCase
-  
+
+  setup do
+    @course = FactoryBot.create(:course)
+  end
+
+  test "Course index page" do
+    post :index
+    assert_response :success
+    assert_select "title", "Courses - Home"
+  end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,43 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  
+  test "Creating a new user" do
+    assert_difference('User.count', +1) do
+      get :create, user: { username: "Helpsme", email: "jiggywithit@gmail.com",
+        last_name: "Kaiserbun", first_name: "Tony", password: "Theword123", role: "admin",
+      password_confirmation: "Theword123" }
+    end
+    assert_redirected_to login_path
+    assert_equal "You signed up successfully", flash[:notice]
+  end
+
+  test "Passwords do not match creating new user" do
+    attrs = FactoryBot.attributes_for(:user)
+    assert_difference('User.count', 0) do
+      get :create, user: { username: attrs[:username], email: attrs[:email],
+        last_name: attrs[:last_name], first_name: attrs[:first_name], password: attrs[:password], role: attrs[:role],
+      password_confirmation: "Theword123" }
+    end
+    assert_response :success
+    assert_equal "Sign up failed. Please fix the errors listed below this form.", flash[:notice]
+  end
+
+  test "Deleting users" do
+    new_user = FactoryBot.create(:user)
+    assert_difference('User.count', -1) do
+      post :destroy, id: new_user[:id]
+    end
+    assert_redirected_to users_list_path
+  end
+
+  test "Updating user" do
+    new_user = FactoryBot.create(:user)
+    assert_difference("User.count", 0) do
+      put :update, id:new_user[:id], user_params: { username: new_user[:username],
+        email: "Hamesbonderrr@gmail.com", last_name: new_user[:last_name], first_name: new_user[:first_name],
+        password: new_user[:password], role: new_user[:role] }
+    end
+    assert_equal "User updated", flash[:notice]
+  end
 
 end
