6f2f64ad4560152c1f8755f5f01e38baa1a3ffc1
vcl2 <vcl2@sfu.ca>
2017-11-20 22:27:25 -0800

    Updated List of Topics page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121023857) do
+ActiveRecord::Schema.define(version: 20171120040603) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 20171121023857) do
     t.string   "first_name"
     t.string   "email"
     t.string   "role"
-    t.string   "tutor"
+    t.boolean  "tutor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
