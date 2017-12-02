22c792d184ab9754bebd31759a219cbe3e77757e
David Ham <dham@sfu.ca>
2017-12-02 15:34:13 -0800

    Uncomment FB login code in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,40 +24,16 @@ class ApplicationController < ActionController::Base
 #  end
 #end
 
-  #def current_user
+  def current_user
 
   #@current_user ||= User.find(session[:user_id]) if session[:user_id]
-  #if $apiflag==1
-  #  @current_user ||= Fbuser.find(session[:user_id]) if session[:user_id]
+  if $apiflag==1
+    @current_user ||= Fbuser.find(session[:user_id]) if session[:user_id]
+  else
 
-  #else
-
-  #def authenticate_user
-  #  if session[:user_id]
-  #     # set current user object to @current_user object variable
-  #    @current_user = User.find session[:user_id]
-  #    return true
-  #  else
-  #    redirect_to login_path #(:controller => 'sessions', :method => 'login')
-  #    return false
-  #  end
-  #end
-
-
-  #def save_login_state
-  #  if session[:user_id]
-  #    #redirect_to(:controller => 'sessions', :method => 'home')
-  #    return false
-  #  else
-  #    return true
-  #  end
-  #end
-
-
-  def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+  end
   helper_method :current_user
 
   def authorize
